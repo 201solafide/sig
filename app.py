ef675cd4f22bcddc1d704cf29acfd87265fb465b
201solafide <solafide.121140201@student.itera.ac.id>
2024-12-07 09:43:59 +0700

    Menambahkan update file

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 # flask app
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__name__":
+    app.run(debug=True)
